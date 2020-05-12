e3cfceb3d3bbb9a0e213c428ea8b54bf4da88ee3
David Wright <davidtwright89@gmail.com>
2020-05-11 22:06:34 -0400

    Moved earlier changes over to the correct view controller

--- VideoRecorder/VideoRecorder/CameraViewController.swift
+++ VideoRecorder/VideoRecorder/CameraViewController.swift
@@ -22,47 +22,6 @@ class CameraViewController: UIViewController {
 		cameraView.videoPlayerLayer.videoGravity = .resizeAspectFill
 	}
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        requestPermissionAndShowCamera()
-    }
-    
-    private func requestPermissionAndShowCamera() {
-        switch AVCaptureDevice.authorizationStatus(for: .video) {
-        case .notDetermined:
-            requestVideoPermission()
-            
-        case .restricted: // Parental controls, for instance, are preventing recording
-            preconditionFailure("Video is disabled, please review device restrictions")
-            
-        case .denied:
-            preconditionFailure("Tell the user they can't use the app without giving permissions via Settings > Privacy > Video")
-            
-        case .authorized:
-            showCamera()
-        
-        @unknown default:
-            preconditionFailure("A new status code was added that we need to handle")
-        }
-    }
-
-    private func requestVideoPermission() {
-        AVCaptureDevice.requestAccess(for: .video) { isGranted in
-            guard isGranted else {
-                preconditionFailure("UI: Tell the user to enable permissions for Video/Camera")
-            }
-            
-            DispatchQueue.main.async {
-                self.showCamera()
-            }
-        }
-    }
-    
-    private func showCamera() {
-        
-    }
-    
     @IBAction func recordButtonPressed(_ sender: Any) {
 
 	}
